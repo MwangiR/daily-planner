84e71099b0241d5c25d0f9e636a11b2efd690da4
Renny <rennymwangi@gmail.com>
2023-06-26 05:51:09 +0930

    Updated hour function to iterate & create div/hr

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const currentDate = dayjs();
-const startTime = currentDate.set("hour", 9).set("minute", 0);
-const endTime = currentDate.set("hour", 17).set("minute", 0);
-
 $(function () {
+  const currentDate = dayjs();
+  const startTime = currentDate.set("hour", 9).set("minute", 0);
+  const endTime = currentDate.set("hour", 17).set("minute", 0);
+
   const rootDivEl = $(".container-fluid");
 
   console.log(rootDivEl);
   console.log(startTime.format("hh:mm A"));
   console.log(endTime.format("hh:mm A"));
+
+  //eventListener
 
   //div element
   for (let i = startTime.hour(); i <= endTime.hour(); i++) {
@@ -19,7 +21,7 @@ $(function () {
 
     const hourDiv = $("<div>")
       .attr({
-        id: `hour-${i}`,
+        id: `hour-${i}-${formattedHour.slice(-2)}`,
         class: "row time-block past",
       })
       .appendTo(rootDivEl);
@@ -53,6 +55,13 @@ $(function () {
       .appendTo(saveBtn);
   }
 
+  $(".time-block button").on("click", function () {
+    const parentEl = $(this).closest(".time-block");
+    const elementId = parentEl.attr("id");
+    const textVal = parentEl.find("textarea").val();
+    console.log(elementId);
+    console.log(textVal);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
